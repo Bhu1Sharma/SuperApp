d1f935837052975ee0d704bfb5aa8d6a181eb799
sahilvrma <sahilverma237340@gmail.com>
2023-05-27 18:06:04 +0530

    this the main shit

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // This is sample application 
 // Create REST endpoint which accepts 2 numbers and returns sum 
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'my_database'
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.error('Failed to establish MySQL connection!', error);
+    return;
+  }
+  
+  console.log('MySQL connection established successfully!');
+});
+
+module.exports = connection;
